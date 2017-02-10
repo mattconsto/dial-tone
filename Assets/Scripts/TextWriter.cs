e0481d93d4ebbb80b77d1c5123a95cca9b712c81
Steffan P <stp1g15@soton.ac.uk>
2017-02-10 23:21:57 +0000

    TextWriter, and a bunch of assets added.

---
+++ Assets/Scripts/TextWriter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextWriter : MonoBehaviour {
+
+	private string toWrite;
+	private int i;
+	public Text text;
+	public AudioSource source;
+
+	// Use this for initialization
+	void Start () {
+		i = 0;
+		Say ("Testing, testing, testing.", Color.red, "left");
+	}
+
+	public void Say (string toSay, Color colour, string allignment) {
+		toWrite = toSay;
+		i = 0;
+		text.text = "";
+		text.color = colour;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (i < toWrite.Length) {
+			text.text += toWrite[i].ToString();
+			i++;
+			source.Play ();
+		}
+
+	}
+}
