32ee2ef9e2d08b915d5a9ac304ea4975844e73c3
gpeh1g14 <gpeh1g14>
2017-02-11 22:33:38 +0000

    seriously cursory cursor this time

---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Hand : MonoBehaviour {
+	public Vector3 offset;
+    // Use this for initialization
+    void Start () {
+        Cursor.visible = false;
+		Update ();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 mousePos = Input.mousePosition;
+		Vector3 worldOffset = transform.TransformPoint (offset);
+		Vector3 posvec = Camera.main.ScreenToWorldPoint (new Vector3 (mousePos.x, mousePos.y, 0));
+		transform.position = new Vector3 (posvec.x, posvec.y, -50) + worldOffset-transform.position;
+    }
+
+}
