c30148ba5ecbb778ed9330fac4dd8a8617e0a40b
Steffan P <stp1g15@soton.ac.uk>
2017-02-11 11:57:05 +0000

    Perfected TextWriter.cs, still needs a nice typewriter sound effect.

--- Assets/Scripts/TextWriter.cs
+++ Assets/Scripts/TextWriter.cs
@@ -6,29 +6,44 @@ using UnityEngine.UI;
 public class TextWriter : MonoBehaviour {
 
 	private string toWrite;
-	private int i;
+	public int cps;
+	private float timeElapsed = 0;
+	public bool speaking = false;
 	public Text text;
 	public AudioSource source;
 
 	// Use this for initialization
 	void Start () {
-		i = 0;
-		Say ("testing, testing, testing", Color.red, "left");
+		Say ("testing, testing, testing", Color.red, "right");
 	}
 
-	public void Say (string toSay, Color colour, string allignment) {
-		toWrite = toSay;
-		i = 0;
-		text.text = "";
-		text.color = colour;
+	public bool Say (string toSay, Color colour, string alignment) {
+		if (!speaking) {
+			speaking = true;
+			toWrite = toSay;
+			timeElapsed = 0f;
+			text.text = "";
+			text.color = colour;
+			if (alignment == "left") {
+				text.alignment = TextAnchor.UpperLeft;
+			}
+			else if (alignment == "right") {
+				text.alignment = TextAnchor.UpperRight;
+			}
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if (i < toWrite.Length) {
-			text.text += toWrite[i].ToString();
-			i++;
+		timeElapsed += Time.deltaTime;
+		text.text = toWrite.Substring (0,Mathf.Min(toWrite.Length, (int)(timeElapsed * cps)));
+		if (text.text == toWrite) {
+			speaking = false;
 		}
-
 	}
+		
 }
