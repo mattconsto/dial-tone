f184d0552c00152c7ae0119e5e636bd8def3d605
Rowan <rp9g14@soton.ac.uk>
2017-02-11 10:09:42 +0000

    non buggy conversation

--- Assets/Scripts/Conversation.cs
+++ Assets/Scripts/Conversation.cs
@@ -27,6 +27,15 @@ public class Conversation {
 		SentanceObject obj = new SentanceObject ();
 		obj.content = str;
 		if (isperson1) {
+<<<<<<< HEAD
+			obj.textColor = person1;
+			obj.Alignment = "left";
+		} else {
+			obj.textColor = person2;
+			obj.Alignment = "right";
+		}
+		return obj;
+=======
 			obj.color = person1;
 			obj.Alignment = "left";
 		} else {
@@ -34,6 +43,7 @@ public class Conversation {
 			obj.Alignment = "right";
 		}
 		return str;
+>>>>>>> d4e65493b8c314a27ebb415389e50d2a8172c2e0
 	}
 	public void reset()
 	{
--- Assets/Scripts/ConversationLoader.cs
+++ Assets/Scripts/ConversationLoader.cs
@@ -6,9 +6,19 @@ using System.Text;
 public class ConversationLoader : MonoBehaviour {
 
 	ArrayList conversations = new ArrayList ();
+	ArrayList drives = new ArrayList ();
+	ArrayList requests = new ArrayList ();
 	void Awake()
 	{
-		loadFromFile();
+		loadFromFile(conversations,"convo.txt");
+		loadFromFile(drives,"drive.txt");
+		loadFromFile(requests,"request.txt");
+	}
+	
+	void loadFromFile(ArrayList list, string filename)
+	{
+		StringBuilder convstr;
+		StreamReader rdr = new StreamReader (Application.dataPath + "/ConversationFiles/" + filename, Encoding.Default);
 	}
 	
 	void loadFromFile()
@@ -24,11 +34,11 @@ public class ConversationLoader : MonoBehaviour {
 				if(line!=null)
 				{
 					if(line.Equals("***"))
-						conversations.Add (new Conversation());
+						list.Add (new Conversation());
 					else
 					{
-						Debug.Log(conversations.Count);
-						((Conversation)conversations[conversations.Count-1]).addSentance(line);
+						((Conversation)list[list.Count-1]).addSentance(line);
+						conversations.Add (new Conversation());
 					}
 
 				}
@@ -36,13 +46,6 @@ public class ConversationLoader : MonoBehaviour {
 			while(line != null);
 			rdr.Close();
 		}
-		/*
-		if (jsonfile.Length != 0) {
-			JSONObject jsonobj = new JSONObject(jsonfile.ToString());
-			//jsonobj.GetField("conversations");
-			accessData(jsonobj);
-		}*/
-		
 	}
 	public Conversation choseRandomConversation()
 	{
@@ -50,6 +53,13 @@ public class ConversationLoader : MonoBehaviour {
 		((Conversation)conversations [convo]).reset ();
 		return (Conversation)conversations [convo];
 	}
+	public Conversation getDriveConversation(int day)
+	{
+		return (Conversation)drives[day];
+	}
+	public Conversation getRequest()
+	{
+		return (Conversation)requests [0];
 
 	void accessData(JSONObject obj){
 		switch(obj.type){
