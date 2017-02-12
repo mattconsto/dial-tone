f974823bc44257730dfeb5eaec0e8845655473fa
Andy <abs1g14@soton.ac.uk>
2017-02-12 00:26:08 +0000

    Feedback Bubbles

--- Assets/Scripts/OnMouse.cs
+++ Assets/Scripts/OnMouse.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class OnMouse : MonoBehaviour {
 	public GameObject toSpawn;
+	public GameObject toSpawn2;
 	bool mdown;
+	bool mdown2;
 	// Use this for initialization
 	void Start () {
 		mdown = Input.GetMouseButtonDown (0);
+		mdown2 = Input.GetMouseButtonDown (1);
 	}
 	
 	// Update is called once per frame
@@ -17,6 +20,13 @@ public class OnMouse : MonoBehaviour {
 			if (mdown == true) {
 				Debug.Log ("YAY");
 				GameObject.Instantiate (toSpawn, Input.mousePosition, new Quaternion (0, 0, 0, 0),GetComponentInParent<Transform>());
+			}
+		}
+		if (Input.GetMouseButtonDown(1) != mdown2) {
+			mdown2 = !mdown2;
+			if (mdown2 == true) {
+				Debug.Log ("YAY");
+				GameObject.Instantiate (toSpawn2, Input.mousePosition, new Quaternion (0, 0, 0, 0),GetComponentInParent<Transform>());
 			}
 		}
 	}
--- Assets/Scripts/bubble.cs
+++ Assets/Scripts/bubble.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class bubble : MonoBehaviour {
 	// Use this for initialization
+	public float upvelocity;
 	void Start () {
 		GetComponent<Image> ().CrossFadeAlpha (0, 1, false);
 		Invoke ("KillMe", 1);
@@ -13,7 +14,7 @@ public class bubble : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		Vector3 mypos = transform.position;
-		transform.position = new Vector3 (mypos.x, mypos.y + 1, mypos.z);
+		transform.position = new Vector3 (mypos.x, mypos.y + upvelocity, mypos.z);
 	}
 
 	void KillMe()
