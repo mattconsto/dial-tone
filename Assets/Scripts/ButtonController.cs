0aee41f83d7b1def29ada52a8d4e96e05db9c307
Steffan P <stp1g15@soton.ac.uk>
2017-02-12 15:14:52 +0000

    Got sheet animations working correctly.

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonController : MonoBehaviour {
+
+    public Button book;
+    public Button sheet;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ToggleBook()
+    {
+        book.interactable = !book.interactable;
+    }
+
+    public void ToggleSheet()
+    {
+        sheet.interactable = !sheet.interactable;
+    }
+
+}
