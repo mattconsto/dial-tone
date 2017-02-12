df734c043137b3017ff49f2174bbcb4a3075ef68
Andy <abs1g14@soton.ac.uk>
2017-02-12 00:07:02 +0000

    Feedback bubbles

---
+++ Assets/Scripts/OnMouse.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnMouse : MonoBehaviour {
+	public GameObject toSpawn;
+	bool mdown;
+	// Use this for initialization
+	void Start () {
+		mdown = Input.GetMouseButtonDown (0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0) != mdown) {
+			mdown = !mdown;
+			if (mdown == true) {
+				Debug.Log ("YAY");
+				GameObject.Instantiate (toSpawn, Input.mousePosition, new Quaternion (0, 0, 0, 0),GetComponentInParent<Transform>());
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/bubble.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class bubble : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		GetComponent<Image> ().CrossFadeAlpha (0, 1, false);
+		Invoke ("KillMe", 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 mypos = transform.position;
+		transform.position = new Vector3 (mypos.x, mypos.y + 1, mypos.z);
+	}
+
+	void KillMe()
+	{
+		GameObject.Destroy (this.gameObject);
+	}
+
+}
